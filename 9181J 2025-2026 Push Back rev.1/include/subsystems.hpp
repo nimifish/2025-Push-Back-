017004e2d435a1a91a7fcaf5042dc7f61f474b7a
viyella voltexes <123225182+ark2000k@users.noreply.github.com>
2025-10-01 15:50:59 -0700

    revision1

---
+++ 9181J 2025-2026 Push Back rev.1/include/subsystems.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+
+extern Drive chassis;
+
+
+inline pros::Motor intakeFirst(20);
+inline pros::Motor intakeThird(11);
+inline pros::Motor_Group intake_group({intakeFirst, intakeThird});
+
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
