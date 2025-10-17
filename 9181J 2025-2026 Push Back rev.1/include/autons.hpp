818637873602deccf5148e0cfb1b3745e2800b39
Ethan L <94648480+nimifish@users.noreply.github.com>
2025-10-17 12:01:35 -0700

    Added in Autonomous Functions

--- 9181J 2025-2026 Push Back rev.1/include/autons.hpp
+++ 9181J 2025-2026 Push Back rev.1/include/autons.hpp
@@ -16,3 +16,9 @@ void odom_pure_pursuit_wait_until_example();
 void odom_boomerang_example();
 void odom_boomerang_injected_pure_pursuit_example();
 void measure_offsets();
+
+void blue_autonomous_right();
+void red_autonomous_left();
+void blue_autonomous_right();
+void red_autonomous_left();
+void skills();
